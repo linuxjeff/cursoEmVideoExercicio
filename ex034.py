098cdf53b053ea229c769bd84c11802fb016cd20
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2022-06-13 13:25:45 -0300

    Criado if para cálcular o aumento. Relatado no cabeçalho.

--- ex034.py
+++ ex034.py
@@ -5,7 +5,14 @@
 #   0.0.1 2022-13-06 Jefferson
 #   - Começo do programa
 #   - Criada as varáveis para recolher nome e salário
+#   - Criado if para cálcular o aumento.
 
 # Variáveis
 NomeDoFuncionario = str(input('Digite o nome do funcionario: ')).capitalize()
 ValorAtualDoSalario = float(input('Qual o valor do salário: R$ '))
+
+# If para efetuar o cálculo
+if  (ValorAtualDoSalario <= 1250.00 ):
+    Aumento = (ValorAtualDoSalario / 100) * 15
+else:
+    Aumento = (ValorAtualDoSalario / 100) * 10
