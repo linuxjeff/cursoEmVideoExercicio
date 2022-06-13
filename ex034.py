2090fa43fcc310fa1a996c23db7447ad1f02813f
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2022-06-13 12:51:19 -0300

    Criada as varáveis para recolher nome e salário. Relatado no cabeçalho.

--- ex034.py
+++ ex034.py
@@ -4,3 +4,8 @@
 #
 #   0.0.1 2022-13-06 Jefferson
 #   - Começo do programa
+#   - Criada as varáveis para recolher nome e salário
+
+# Variáveis
+NomeDoFuncionario = str(input('Digite o nome do funcionario: ')).capitalize()
+ValorAtualDoSalario = float(input('Qual o valor do salário: R$ '))
