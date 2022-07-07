d8334bdc69a53bdf04f1a7c342198091210d91ad
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2022-07-07 20:08:33 -0300

    Criado as variáveis. Relatado no cabeçalho.

--- ex035.py
+++ ex035.py
@@ -4,3 +4,9 @@
 #
 #   0.0.1 2022-07-07 Jefferson
 #   - Começo do programa
+#   - Criado as variáveis
+
+# Variáveis
+RetaUm = float(input('Digite o tamanho da primeira reta: '))
+RetaDois = float(input('Digite o tamanho da segunda reta: '))
+RetaTres = float(input('Digite o tamanho da terceira reta: '))
